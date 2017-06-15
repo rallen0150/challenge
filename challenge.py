e4a5e90bba947565694e289a72e249bc87256412
rallen0150 <rallen0150@gmail.com>
2017-06-15 14:26:40 -0400

    challenge for company

--- challenge.py
+++ challenge.py
@@ -1,9 +1,13 @@
 arr = [4, 1, -16, 3.14, 5]
 # min = -16, 1
 # max = 4, 5
+n=2
 
 def min_max(arr, n):
-    return sorted(arr)[:n][-n:]
+    return sorted(arr)[:n][-n:] #Or this way
+    # arr = sorted(arr)
     # min_value = arr[:n]
     # max_value = arr[-n:]
     # return min_value, max_value
+
+print(min_max(arr, n))
